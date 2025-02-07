0d49a1eaff5c4f3d2225e897a72b393dda2deb56
DCS User <DCSUser@cn.lan>
2025-02-07 12:06:00 +0530

    api end point update

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\productController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/products', [productController::class, 'index']);
